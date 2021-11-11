f3e131d445f3a75e23c59b933b44c2b6a02b4857
kkthmyh <34261479+kkthmyh@users.noreply.github.com>
2021-11-11 10:19:18 +0800

    Update lib.rs
    update

--- pallets/poe/src/lib.rs
+++ pallets/poe/src/lib.rs
@@ -10,7 +10,7 @@ pub mod pallet{
 	use frame_system::pallet_prelude::{BlockNumberFor, OriginFor};
 	use frame_system::ensure_signed;
 	use sp_std::vec::Vec;
-
+	/// 配置
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
@@ -19,12 +19,13 @@ pub mod pallet{
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
-
+	
+	/// 存储
 	#[pallet::storage]
 	#[pallet::getter(fn proofs)]
 	pub type Proofs<T:Config> = StorageMap<_, Blake2_128Concat,Vec<u8>,(T::AccountId,T::BlockNumber)>;
 
-
+	/// 事件
 	#[pallet::event]
 	#[pallet::metadata(T::AccountId = "AccountId")]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -33,7 +34,7 @@ pub mod pallet{
 		ClaimRevoked(T::AccountId,Vec<u8>),
 	}
 
-
+	/// error处理
 	#[pallet::error]
 	pub enum Error<T> {
 		ProofAlreadyExist,
@@ -50,9 +51,13 @@ pub mod pallet{
 		#[pallet::weight(0)]
 		/// 创建存证
 		pub fn create_claim(origin:OriginFor<T>,claim:Vec<u8>)->DispatchResultWithPostInfo{
+			// 判断是否是签名用户
 			let sender = ensure_signed(origin)?;
+			// 判断存证是否已经存在
 			ensure!(!Proofs::<T>::contains_key(&claim),Error::<T>::ProofAlreadyExist);
+			// 若不存在进行insert
 			Proofs::<T>::insert(&claim,(sender.clone(),frame_system::Pallet::<T>::block_number()));
+			// 触发create事件
 			Self::deposit_event(Event::ClaimCreated(sender,claim));
 			Ok(().into())
 		}
@@ -60,10 +65,15 @@ pub mod pallet{
 		#[pallet::weight(0)]
 		/// 撤销存证
 		pub fn revoke_claim(origin:OriginFor<T>,claim:Vec<u8>)->DispatchResultWithPostInfo{
+			// 判断是否是签名用户
 			let sender = ensure_signed(origin)?;
+			// 获取当前存证的所有者
 			let (owner,_) = Proofs::<T>::get(&claim).ok_or(Error::<T>::ClaimNotExist)?;
+			// 只有所有者才可以撤销存证
 			ensure!(owner == sender,Error::<T>::NotClaimOwner);
+			// 撤销操作
 			Proofs::<T>::remove(&claim);
+			// 触发revoke事件
 			Self::deposit_event(Event::ClaimRevoked(sender,claim));
 			Ok(().into())
 		}
